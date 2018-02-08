8650856eec69adb38e0a8d45eb494db430755ca4
Lewis Lepton <lewisleptondev@gmail.com>
2018-02-08 12:28:45 +0000

    updated with keyCode + minor updates

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -4,7 +4,7 @@ import kha.System;
 
 class Main {
 	public static function main() {
-		System.init({title: "kha-pong", width: 1024, height: 768}, function () {
+		System.init({title: "KHA PONG", width: 1024, height: 768}, function () {
 			kha.Assets.loadEverything(function() {
 				new Project();
 			});
--- khafile.js
+++ khafile.js
@@ -1,4 +1,4 @@
-var project = new Project('New Project');
+let project = new Project('KHA PONG');
 project.addAssets('Assets/**');
 project.addSources('Sources');
-return project;
+resolve(project);
